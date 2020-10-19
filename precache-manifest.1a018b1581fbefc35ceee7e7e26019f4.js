d565f7eb4191b7341043b8f28181b3383b55fd9e
Agustin Vignolo <agus88lp@gmail.com>
2020-10-19 19:35:31 -0300

    Updates

---
+++ precache-manifest.1a018b1581fbefc35ceee7e7e26019f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1078b594b091847fc862c8be1b9f687a",
+    "url": "/react-tateti/index.html"
+  },
+  {
+    "revision": "25ccd5b7720635bd122a",
+    "url": "/react-tateti/static/css/main.c49af987.chunk.css"
+  },
+  {
+    "revision": "3f7083e6c4b6cfe2113e",
+    "url": "/react-tateti/static/js/2.df29fb57.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-tateti/static/js/2.df29fb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ccd5b7720635bd122a",
+    "url": "/react-tateti/static/js/main.aef2f1f5.chunk.js"
+  },
+  {
+    "revision": "df69ee9e4da15b00319c",
+    "url": "/react-tateti/static/js/runtime-main.11e35204.js"
+  }
+]);
